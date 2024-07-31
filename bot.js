1a0bba0d6b70940b07aa37a67586a78aa341b9da
danialzadrafiee <leylasarafrezai@gmail.com>
2024-07-31 00:03:53 -0700

    update

--- bot.js
+++ bot.js
@@ -3,16 +3,18 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '7053995358:AAGt8HsL5epgQZRdTI4sou7SV8mU0ES3cdA';
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/game/, (msg) => {
+// Handle the /start command
+bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const gameUrl = 'https://tower.developerpie.com/dist/index.html';
   
-  bot.sendPhoto(chatId, 'https://play-lh.googleusercontent.com/Ai4Ld0NsDjBi_TZwJ2KBSWFfmN_GJ9ckesdC3q1mXvzdiouAqgbwj6LoCvRn0x3rWMs', {
-    caption: '🎮 Welcome to Our Exciting Game! 🎮\n\nReady to begin your adventure?',
+  bot.sendMessage(chatId, '🎮 Welcome to Our Exciting Game! 🎮\n\nReady to begin your adventure?', {
     reply_markup: {
-      inline_keyboard: [[
-        { text: "▶️ Start Game", web_app: { url: gameUrl } }
-      ]]
+      keyboard: [[
+        { text: "Start Game", web_app: { url: gameUrl } }
+      ]],
+      resize_keyboard: true,
+      one_time_keyboard: false
     }
   });
 });
